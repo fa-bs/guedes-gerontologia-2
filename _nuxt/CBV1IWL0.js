d302ec0f3a5917e50736a73686ebb44a147fa3ff
fa-bs <0.fabiobs@gmail.com>
2024-07-23 23:15:45 -0300

    site review 2

---
+++ _nuxt/CBV1IWL0.js
@@ -0,0 +1,1 @@
+import"./Bpj50gRG.js";const t=""+new URL("brain-insight.C6W1V-6o.svg",import.meta.url).href;export{t as _};
---
+++ _nuxt/DXkTakBH.js
@@ -0,0 +1,1 @@
+import"./Bpj50gRG.js";const e=""+new URL("logo-guedes-gerontologia.BabNepvb.svg",import.meta.url).href;export{e as _};
